9114ee1650a973995c0f9131dc67c932101988ca
RubenTipparach_cp <SND\RubenTipparach_cp>
2014-08-13 23:35:13 +0000

    Initial Check in of Solution

---
+++ FleetHackers/FleetHackers/FleetHackers/Cameras/TargetCamera.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FleetHackers.Cameras
+{
+	public class TargetCamera : AbstractCamera
+	{
+		/// <summary>
+		/// Default constructor for the target camera.
+		/// </summary>
+		/// <param name="graphicsDevice">Graphics device object.</param>
+		public TargetCamera(Vector3 position, Vector3 target, GraphicsDevice graphicsDevice)
+			: base(graphicsDevice)
+		{
+			this.Position = position;
+			this.Target = target;
+		}
+
+		/// <summary>
+		/// Synchronize this camera update with the rest of the game.
+		/// </summary>
+		public override void Update()
+		{
+			Vector3 forward = Target - Position;
+			Vector3 side = Vector3.Cross(forward, Vector3.Up);
+			Vector3 up = Vector3.Cross(side, forward);
+			this.View = Matrix.CreateLookAt(Position, Target, up);
+		}
+
+		/// <summary>
+		/// Position of the camera.
+		/// </summary>
+		public Vector3 Position
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// The target for this camera.
+		/// </summary>
+		public Vector3 Target
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ FleetHackers/FleetHackers/FleetHackers/Models/BasicModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FleetHackers.Models
+{
+	public class BasicModel
+	{
+		/// <summary>
+		/// Used to hold transformation operations.
+		/// </summary>
+		private Matrix[] modelTransforms;
+
+		/// <summary>
+		/// Graphics device for controling graphics. Modularizing this class.
+		/// </summary>
+		private GraphicsDevice graphicsDevice;
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		/// <param name="model">Pass in a loaded model object.</param>
+		/// <param name="position">Position of the object.</param>
+		/// <param name="Rotation">Orientation of the object.</param>
+		/// <param name="Scale">The size of the object with relation to its absolute size.</param>
+		/// <param name="graphicsDevice">Allows contol of the graphics device object.</param>
+		public BasicModel(Model model, Vector3 position, Vector3 rotation, Vector3 scale, GraphicsDevice graphicsDevice)
+		{
+			this.Model = model;
+			this.Position = position;
+			this.Rotation = rotation;
+			this.Scale = scale;
+			this.graphicsDevice = graphicsDevice;
+
+			modelTransforms = new Matrix[model.Bones.Count];
+			model.CopyAbsoluteBoneTransformsTo(modelTransforms);
+		}
+
+		/// <summary>
+		/// This method is responsible for allowing the Model to render itself.
+		/// TO DO: We can make this into some kind of interface.
+		/// </summary>
+		/// <param name="view">Camera postion and rotation.</param>
+		/// <param name="projection">Camera viewing angle and depth.</param>
+		public void Draw(Matrix view, Matrix projection)
+		{
+			Matrix baseWorld = Matrix.CreateScale(Scale)
+				* Matrix.CreateFromYawPitchRoll(Rotation.Y, Rotation.X, Rotation.Z)
+				* Matrix.CreateTranslation(Position);
+
+			foreach (ModelMesh mesh in Model.Meshes)
+			{
+				Matrix localWorld = modelTransforms[mesh.ParentBone.Index] * baseWorld;
+
+				foreach ( ModelMeshPart meshpart in mesh.MeshParts)
+				{
+					BasicEffect effect = (BasicEffect)meshpart.Effect;
+					effect.World = localWorld;
+					effect.View = view;
+					effect.Projection = projection;
+
+					effect.EnableDefaultLighting();
+				}
+
+				mesh.Draw();
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets postion of the model.
+		/// </summary>
+		public Vector3 Position
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the rotation of the model.
+		/// </summary>
+		public Vector3 Rotation
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the scale.
+		/// </summary>
+		public Vector3 Scale
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the model object.
+		/// </summary>
+		public Model Model
+		{
+			get;
+			set;
+		}
+
+	}
+}
