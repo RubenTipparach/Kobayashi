ef696cc320e6411c5ce6916ff3ce11f809db4cf6
jceddy_cp <SND\jceddy_cp>
2014-09-02 19:07:32 +0000

    Added Bounce and Discard effects, added a card that uses them.

---
+++ FleetHackers/FleetHackers/FleetHackers/Cards/Effects/BounceEffect.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using FleetHackers.Cards.Effects.Enums;
+using FleetHackers.Cards.Enums;
+
+namespace FleetHackers.Cards.Effects
+{
+	[DataContract]
+	public class BounceEffect : Effect
+	{
+		public override EffectType EffectType
+		{
+			get
+			{
+				return EffectType.Bounce;
+			}
+		}
+
+		public Target Target { get; set; }
+
+		[DataMember(Name = "target")]
+		public string TargetString
+		{
+			get
+			{
+				return Target.ToString();
+			}
+			set
+			{
+				Target = (Target)Enum.Parse(typeof(Target), value);
+			}
+		}
+
+		public override string ToString(Card card, bool capitalize = false)
+		{
+			StringBuilder toStringBuilder = new StringBuilder();
+
+			if (capitalize)
+			{
+				toStringBuilder.Append("Return ");
+			}
+			else
+			{
+				toStringBuilder.Append("return ");
+			}
+
+			switch (Target)
+			{
+				case Target.NonInfluence:
+					toStringBuilder.Append("target non-influence card ");
+					break;
+				default:
+					throw new InvalidOperationException("Unsupported Target for BounceEffect.");
+			}
+
+			toStringBuilder.Append("to its owner's hand");
+
+			return toStringBuilder.ToString();
+		}
+	}
+}
---
+++ FleetHackers/FleetHackers/FleetHackers/Cards/Effects/DiscardEffect.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using FleetHackers.Cards.Enums;
+using FleetHackers.Cards.Effects.Enums;
+
+namespace FleetHackers.Cards.Effects
+{
+	[DataContract]
+	public class DiscardEffect : Effect
+	{
+		public override EffectType EffectType
+		{
+			get
+			{
+				return EffectType.Bounce;
+			}
+		}
+
+		public Target Target { get; set; }
+
+		[DataMember(Name = "target")]
+		public string TargetString
+		{
+			get
+			{
+				return Target.ToString();
+			}
+			set
+			{
+				Target = (Target)Enum.Parse(typeof(Target), value);
+			}
+		}
+
+		public ChooseMethod ChooseMethod { get; set; }
+
+		[DataMember(Name = "chooseMethod")]
+		public string ChooseMethodString
+		{
+			get
+			{
+				return ChooseMethod.ToString();
+			}
+			set
+			{
+				ChooseMethod = (ChooseMethod)Enum.Parse(typeof(ChooseMethod), value);
+			}
+		}
+
+		public override string ToString(Card card, bool capitalize = false)
+		{
+			StringBuilder toStringBuilder = new StringBuilder();
+
+			switch (Target)
+			{
+				case Target.Opponent:
+					switch (ChooseMethod)
+					{
+						case ChooseMethod.YouChoose:
+							if (capitalize)
+							{
+								toStringBuilder.Append("L");
+							}
+							else
+							{
+								toStringBuilder.Append("l");
+							}
+							toStringBuilder.Append("ook at target opponent's and and choose a card in it. That player discards the chosen card");
+							break;
+						default:
+							throw new InvalidOperationException("Unsupported ChooseMethod for DiscardEffect.");
+					}
+					break;
+				default:
+					throw new InvalidOperationException("Unsupported Target for DiscardEffect.");
+			}
+
+			return toStringBuilder.ToString();
+		}
+	}
+}
