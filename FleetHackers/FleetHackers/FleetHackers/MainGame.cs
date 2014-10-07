ba77852d35b543e68ae2c8c90f4a2a0a9157b04e
jceddy_cp <SND\jceddy_cp>
2014-10-07 21:48:00 +0000

    Changed TutorEffect text to reference the influence pool when tutoring for influence cards.

--- FleetHackers/FleetHackers/FleetHackers/MainGame.cs
+++ FleetHackers/FleetHackers/FleetHackers/MainGame.cs
@@ -424,7 +424,7 @@ namespace FleetHackers
 				/*Card c = null;
 				foreach (Card cx in _cards)
 				{
-					if (cx.Title == "Stolen Intentions")
+					if (cx.Title == "Gaining Influence")
 					//if (_rulesTextFont.MeasureString(WrapText(_rulesTextFont, cx.RulesText, _cardRulesWidth)).Y > _cardRulesHeight)
 					{
 						c = cx;
